fc0290ef3a31ddf0e5b5c2caf75e16a767ca99dd
gamingexpx12 <gamingexpx12@gmail.com>
2016-10-31 14:45:22 +0100

    Tk fix

--- ViperTk.py
+++ ViperTk.py
@@ -4,5 +4,5 @@ def HelloBox():
     tkMessageBox.showinfo(title="Titular Title", message="Hello Python, Goodbye spam!")
 # Code to add widgets will go here...
 B = Tkinter.Button( top, text = "Hello World", command = HelloBox )
-B.pack()
+#B.pack() #What does this even do?
 top.mainloop()
