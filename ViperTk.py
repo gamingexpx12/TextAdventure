32880bf81c6721026b428eed030cbda67aaee52e
gamingexpx12 <gamingexpx12@gmail.com>
2016-10-31 13:59:53 +0100

    Addded Tk api

---
+++ ViperTk.py
@@ -0,0 +1,8 @@
+import Tkinter, tkMessageBox
+top = Tkinter.Tk()
+def HelloBox():
+    tkMessageBox.showinfo(title="Titular Title", message="Hello Python, Goodbye spam!")
+# Code to add widgets will go here...
+B = Tkinter.Button( top, text = "Hello World", command = HelloBox )
+B.pack()
+top.mainloop()
